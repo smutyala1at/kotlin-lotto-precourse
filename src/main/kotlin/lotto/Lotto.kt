fbd8fd84d631fede6a47d20b550cfae5e2fe6efb
Santosh Mutyala <santosh.mutyala.dev@gmail.com>
2025-04-28 22:09:05 +0200

    feat: extend Lotto class with validations and implement InputView class

--- src/main/kotlin/lotto/Lotto.kt
+++ src/main/kotlin/lotto/Lotto.kt
@@ -3,7 +3,15 @@ package lotto
 class Lotto(private val numbers: List<Int>) {
     init {
         require(numbers.size == 6) { "[ERROR] Lotto must contain exactly 6 numbers." }
+        require(numbers.all { it in 1..45 }) { "[ERROR] Lotto numbers must be between 1 and 45." }
+        require(numbers.distinct().size == numbers.size) { "[ERROR] Lotto numbers must be unique." }
     }
 
-    // TODO: Implement additional functions
+    fun getNumbers(): List<Int> {
+        return numbers.toList()
+    }
+
+    fun contains(number: Int): Boolean {
+        return numbers.contains(number)
+    }
 }
---
+++ src/main/kotlin/lotto/io/InputView.kt
@@ -0,0 +1,68 @@
+package lotto.io
+
+import camp.nextstep.edu.missionutils.Console
+import lotto.Lotto
+
+class InputView {
+    fun readPurchaseAmount(): Int {
+        println("Please enter the purchase amount.")
+        val input = Console.readLine()
+
+        /* parse input to int and validate */
+        val amount = try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] Please enter a valid number.")
+        }
+
+        if (amount <= 0) {
+            throw IllegalArgumentException("[ERROR] The purchase amount must be greater than zero.")
+        }
+
+        if(amount % TICKET_PRICE != 0) {
+            throw IllegalArgumentException("[ERROR] The purchase price must be a positive number.")
+        }
+
+        return amount
+    }
+
+    fun readWinningNumbers(): Lotto {
+        println("\nPLease enter last week's winning numbers.")
+        val input = Console.readLine()
+
+        try {
+            val numbers = input.split(",").map { number -> number.trim().toInt() }.toList()
+            return Lotto(numbers)
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] Please enter valid numbers separated by commas.")
+        }
+    }
+
+    fun readBonusNumber(winningNumbers: Lotto): Int {
+        println("\nPLease enter the bonus number.")
+        val input = Console.readLine()
+
+        /* parse input to int and validate */
+        val bonusNumber = try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("[ERROR] Please enter a valid bonus number. Should be distinct from winning numbers.")
+        }
+
+        if (bonusNumber < 1 || bonusNumber > 45) {
+            throw IllegalArgumentException("[ERROR] Bonus number must be between 1 and 45.")
+        }
+
+        if (winningNumbers.contains(bonusNumber)) {
+            throw IllegalArgumentException("[ERROR] Bonus number cannot be one of the winning numbers.")
+        }
+
+        return bonusNumber
+
+    }
+
+
+    companion object {
+        private const val TICKET_PRICE = 1000
+    }
+}
